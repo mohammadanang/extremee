33e1540f9e9edc8a0f0c3699339c55ec0a3ccdd4
mohammadanang <m.anangnur@gmail.com>
2020-11-27 21:56:00 +0700

    add dotenv package & implement it

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use(cors());
 
@@ -8,6 +12,6 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
-app.listen(8000, () => {
-    console.log("Server started on port: 8000");
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
 });
